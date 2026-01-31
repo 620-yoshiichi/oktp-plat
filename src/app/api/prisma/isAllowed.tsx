9b2bac381d65079961657b5130790e5509c30f1a
620-yoshiichi <mutsuo_yoshiichi@toch-holdings.com>
2026-01-31 14:52:34 +0900

    fix

--- src/app/api/prisma/isAllowed.tsx
+++ src/app/api/prisma/isAllowed.tsx
@@ -20,7 +20,7 @@ const checkAccess = (host: string | null, authorization: string | null): boolean
 export const isRouteAccessAllowed = async (req: NextRequest) => {
   const host = req.headers.get('x-forwarded-host') ?? req.headers.get('host')
   const authorization = req.headers.get('authorization')
-  return checkAccess(host, authorization)
+  return checkAccess(host, authorization) || await isByCronJob()
 }
 
 /**
@@ -31,11 +31,19 @@ export const isServerActionAccessAllowed = async (): Promise<boolean> => {
   const headersList = await headers()
   const host = headersList.get('x-forwarded-host') ?? headersList.get('host')
   const authorization = headersList.get('authorization')
-  return checkAccess(host, authorization)
+  return checkAccess(host, authorization) || await isByCronJob()
 }
 
 export const logObject = (obj: anyObject) => {
   let stringified = JSON.stringify(obj)
   stringified = stringified.replace(/\s+/g, ' ')
   return stringified
+}
+
+
+const isByCronJob = async () => {
+  const headersList = await headers()
+  const byCronJob = headersList.get('Authorization') === `Bearer ${process.env.CRON_SECRET}`
+
+  return byCronJob
 }
